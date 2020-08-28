31f64652fef7882aff1953044bb0617bcf63fcc4
Codel1417 <austinrichards98@gmail.com>
2020-08-28 06:11:13 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 # https://stackoverflow.com/questions/32076878/logging-solution-for-multiple-containers-running-on-same-host
 
 nohup bash -c "dockerd --host=unix:///var/run/docker.sock" \
+sleep 15
 docker-compose up  --no-start  --no-recreate
 cd ~/TheSpaghettiDetective
 docker-compose start
